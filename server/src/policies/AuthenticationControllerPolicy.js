218a9520fbde47851f7b20c75fc0adc00103fedd
Irina <irina060981@yandex.ru>
2017-12-22 18:46:57 +1000

    turn on validating

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,40 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9а-яА-Я]{8,32}$')
+      )
+    }
+
+    const {error, value} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: `The password provided failed to match the following rules:
+              <ol>
+              <li>It must contain ONLY the following characters: lower case, Upper case and numbers.</li>
+              <li>It must be at least 8 characters in length and not greater than 32 characters in length.</li>
+              </ol>
+            `
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}
