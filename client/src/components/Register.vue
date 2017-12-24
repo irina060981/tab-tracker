c2176624ee6ce8a9b9ec8ff1daf1cb15b4853270
Irina <irina060981@yandex.ru>
2017-12-25 09:48:17 +1000

    add css framework vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="register">
-    <h1>Register</h1>
-    <p><input type="email" name="email" placeholder="email" v-model="email" /></p>
-    <p><input type="password" name="password" placeholder="password" v-model="password" /></p>
-    <div class="error" v-html="error"></div>
-    <p><button @click="register">Register</button></p>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class = "white elevation-2">
+
+        <v-toolbar flat dense class="green" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <p><input type="email" name="email" placeholder="email" v-model="email" /></p>
+          <p><input type="password" name="password" placeholder="password" v-model="password" /></p>
+          <div class="error" v-html="error"></div>
+          <p><v-btn class="green" @click="register">Register</v-btn></p>
+        </div>
+
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
