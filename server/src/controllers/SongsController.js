19fca56a12ddcc7f397181cd1f0a4fd61ee9d6f4
Irina <irina060981@yandex.ru>
2017-12-29 16:37:07 +1000

    server side search

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -4,10 +4,26 @@ const config = require('../config/config')
 module.exports = {
   async index (req, res) {
     try {
-      const song = await Song.findAll({
-        limit: 10
-      })
-      res.send(song)
+      let songs = null
+      let search = req.query.search
+
+      if (search) {
+        songs = await Song.findAll({
+          where: {
+            $or: [
+              'title', 'artist', 'genre', 'album'
+            ].map(key => ({ // преобразуем в объект
+              [key]: { $like: `%${search}%` }
+            }))
+          }
+        })
+      } else {
+        songs = await Song.findAll({
+          limit: 10
+        })
+      }
+
+      res.send(songs)
     } catch (err) {
       res.status(500).send({
         error: 'An error has occured trying to fetched songs'
